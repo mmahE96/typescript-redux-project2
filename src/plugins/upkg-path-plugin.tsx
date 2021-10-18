3088614b692b779e72e8e8642213f0f4a0feaf36
Mahir Mulaomerovic <mahir@192.168.1.11>
2021-10-18 17:25:28 +0200

    plugin updated

--- src/plugins/upkg-path-plugin.tsx
+++ src/plugins/upkg-path-plugin.tsx
@@ -1,7 +1,12 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localforage from 'localforage';
 
-export const unpkgPathPlugin = () => {
+const fileCache = localforage.createInstance({
+    name: 'filecache',
+  });
+
+export const unpkgPathPlugin = (inputCode:string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -33,19 +38,25 @@ export const unpkgPathPlugin = () => {
           if (args.path === 'index.js') {
             return {
               loader: 'jsx',
-              contents: `
-                const message = require('nested-test-pkg');
-                console.log(message);
-              `,
+              contents: inputCode,
             };
+          }
+
+          const cachedResult = await fileCache.getItem<esbuild.OnLoadArgs>(args.path)
+
+          if (cachedResult){
+              return cachedResult;
           }
   
           const { data, request }:any = await axios.get(args.path);
-          return {
+
+          const result = {
             loader: 'jsx',
             contents: data,
             resolveDir: new URL('./', request.responseURL).pathname,
           };
+
+          await fileCache.setItem(args.path, result)
         });
       },
     };
