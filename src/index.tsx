7a6c98cad84db2a05557f91b7e487f9535ea7234
Mahir Mulaomerovic <mahir@Mahirs-MacBook-Pro.local>
2021-10-18 13:17:15 +0200

    Added plugin for modifying esbuild. Esbuild now compile and transpile in browser with option of adding npm packages from unpkg.com

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import { useState, useEffect, useRef } from "react";
+import { unpkgPathPlugin } from "./plugins/upkg-path-plugin";
+import * as esbuild from "esbuild-wasm"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+
+    const ref = useRef<any>()
+    const [input, setInput] = useState("")
+    const [code, setCode] = useState("")
+
+    useEffect(() => {
+        startService()
+        
+    }, [])
+
+    const startService = async () => {
+
+        ref.current = await esbuild.startService({
+            worker:true,
+            wasmURL:"/esbuild.wasm"
+        })
+        
+
+    }
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define:{
+          "process.env.NODE_ENV": '"production"',
+          global: "window",
+          
+      }
+    });
+
+    // console.log(result);
+
+    setCode(result.outputFiles[0].text);
+  };
+    
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App/>, document.querySelector("#root"));
